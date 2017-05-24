ff62732d702edce265d46499cc627fe3823c7707
zhoujunjiang <785375121@qq.com>
2017-05-24 14:41:31 +0800

    初步完成整个应用

--- app/src/main/java/com/aidchow/mm36d/ui/SettingActivity.java
+++ app/src/main/java/com/aidchow/mm36d/ui/SettingActivity.java
@@ -1,11 +1,14 @@
 package com.aidchow.mm36d.ui;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.aidchow.mm36d.R;
+import com.mikepenz.aboutlibraries.LibsBuilder;
+import com.mikepenz.aboutlibraries.ui.LibsSupportFragment;
 
 
 public class SettingActivity extends BaseActivity {
@@ -14,7 +17,12 @@ public class SettingActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_category);
-        initToolbar(getString(R.id.menu_setting));
+        initToolbar(getString(R.string.menu_setting));
+        LibsSupportFragment fragment = new LibsBuilder()
+                .withLibraries("crouton")
+                .supportFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().add(R.id.frame_content, fragment).commit();
     }
 
 
